b9cd07750d51fda54954789c0b9f61f4c8f8998e
Yakir <yakirgubi@gmail.com>
2023-05-15 21:05:28 +0300

    Getter and Setter

--- src/Coin.java
+++ src/Coin.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Random;
 
 public class Coin {
     private int x;
@@ -10,11 +11,56 @@ public class Coin {
         this.x = x;
         this.y = y;
     }
+//    public static void replace(Player player1 , Player player2 , Wall[] walls){
+//        Random random = new Random();
+//        int x;
+//        int y;
+//
+//        do{
+//           x = random.nextInt();
+//           y = random.nextInt();
+//        }while
+//    }
+//    public static boolean isCollision(Player player1 , Player player2 , Wall[] walls){
+//        return false;
+//    }
 
     public void paint(Graphics graphics) {
 
         graphics.setColor(this.color);
         graphics.fillOval(this.x, this.y, this.size, this.size);
 
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
